aa79d8d77c1aea967eabec56b1389c2a587d3308
Remilia172112 <trangianguyen.com@gmail.com>
2023-11-05 19:41:20 +0700

    update login + css

---
+++ js/login.js
@@ -0,0 +1,8 @@
+let login_form_total = document.getElementById('form_login_total');
+
+function open_login() {
+    login_form_total.style.display = 'flex';
+}
+function close_login() {
+    login_form_total.style.display = 'none';
+}
